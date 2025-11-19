ee3a31d50c08ceda046f7c2fccd6e6a6f39559bd
VeronicahWanjuu <v.wanjuu@alustudent.com>
2025-11-19 08:00:01 +0200

    fix(backend): Resolve all SQLAlchemy migration and mapper issues

--- backend/models/user.py
+++ backend/models/user.py
@@ -1,8 +1,6 @@
 from datetime import datetime
 from werkzeug.security import generate_password_hash, check_password_hash
-from flask_sqlalchemy import SQLAlchemy
-
-db = SQLAlchemy()
+from . import db
 
 class User(db.Model):
     """
